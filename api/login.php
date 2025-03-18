020c29a165a2b707b820de0f75ce6fda1960e88a
JdM <julien@rmbi.ch>
2025-03-18 21:05:04 +0100

    lot of things

--- api/login.php
+++ api/login.php
@@ -1,5 +1,7 @@
 <?php
+error_reporting(E_ALL);
 include_once(dirname(__FILE__) . "/utils/database.php");
+session_start();
 
 if (isset($_SESSION['user_id'])) {
     echo json_encode(array("success" => false, "error" => "Already logged in"));
@@ -12,18 +14,17 @@ $db = new Database;
 $username = htmlspecialchars($_POST['username']);
 $password = $_POST['password'];
 
-$users = $db.select("SELECT * FROM users WHERE username = ?", [$username]);
+$users = $db -> select("SELECT * FROM users WHERE username = ?", [$username]);
 if (count($users) == 0) {
     echo json_encode(array("success" => false, "error" => "Username does not exist"));
     exit();
 }
 $user = $users[0];
-if (!password_verify($password, $user['password'])) {
+if (!password_verify($password, $user['passwrd'])) {
     echo json_encode(array("success" => false, "error" => "Incorrect password"));
     exit();
 }
 
-session_start();
 $_SESSION['user_id'] = $user['id'];
 echo json_encode(array("success" => true));
 exit();
--- api/register.php
+++ api/register.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+session_start();
+
 include_once(dirname(__FILE__) . "/utils/database.php");
 
 if (isset($_SESSION['user_id'])) {
@@ -13,7 +16,7 @@ $username = htmlspecialchars($_POST['username']);
 $password = $_POST['password'];
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$users = $db.select("SELECT * FROM users WHERE username = ?", [$username]);
+$users = $db -> select("SELECT * FROM users WHERE username = ?", [$username]);
 if (count($users) > 0) {
     echo json_encode(array("success" => false, "error" => "Username already exists"));
     exit();
@@ -22,22 +25,21 @@ if (strlen($username) < 3) {
     echo json_encode(array("success" => false, "error" => "Username must be at least 3 characters"));
     exit();
 }
-if (strlen($username > 20)) {
+if (strlen($username) > 20) {
     echo json_encode(array("success" => false, "error" => "Username must be at most 20 characters"));
     exit();
 }
 
-$namePpattern = "[a-zA-Z0-9]+";
+$namePattern = "/^[a-zA-Z0-9]+$/";
 if (!preg_match($namePattern, $username)) {
-    echo json_encode(array("success" => false, "error" => "Username must be alphanumeric"));
+    echo json_encode(array("success" => false, "error" => "Username must contain only letters and numbers"));
     exit();
 }
 
-$db.query("INSERT INTO users (username, passwrd) VALUES (?, ?)", [$username, $hashed_password]);
-$users = $db.select("SELECT * FROM users WHERE username = ?", [$username]);
+$db -> query("INSERT INTO users (username, passwrd) VALUES (?, ?)", [$username, $hashed_password]);
+$users = $db -> select("SELECT * FROM users WHERE username = ?", [$username]);
 $user = $users[0];
 
-session_start();
 $_SESSION['user_id'] = $user['id'];
 
 echo json_encode(array("success" => true));
